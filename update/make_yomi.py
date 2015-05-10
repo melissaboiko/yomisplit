34cb4cdfa01ae47b6852591165f257b889d1922e
Leonardo Boiko <leoboiko@namakajiri.net>
2015-05-10 13:41:29 -0300

    initial commit

---
+++ update/make_yomi.py
@@ -0,0 +1,48 @@
+#!/usr/bin/env python3
+import os
+import jctconv
+
+os.chdir(os.path.dirname(os.path.abspath(__file__)))
+
+onyomis = {}
+onyomi_tsv = open('onyomi.tsv', 'r')
+for line in onyomi_tsv:
+    fields = line.strip().split("\t")
+    kanji = fields[0]
+
+    if len(fields) < 2:
+        onyomis[kanji] = []
+        continue
+        
+    ons = fields[1].split()
+    ons = [jctconv.kata2hira(on) for on in ons]
+    # should we use this information (bound readings) somehow?
+    ons = [on.replace('-', '') for on in ons]
+    ons = [on.strip() for on in ons]
+    onyomis[kanji] = ons
+
+kunyomis = {}
+kunyomi_tsv = open('kunyomi.tsv', 'r')
+for line in kunyomi_tsv:
+    fields = line.strip().split("\t")
+    kanji = fields[0]
+
+    if len(fields) < 2:
+        kunyomis[kanji] = []
+        continue
+
+    kuns = fields[1].split()
+
+    # 'たべ.る' → 'たべ'
+    for idx, kun in enumerate(kuns):
+        dot = kun.find('.')
+        if dot != -1:
+            kuns[idx] = kun[0:dot]
+
+    # should we use this information (bound readings) somehow?
+    kuns = [kun.replace('-', '') for kun in kuns]
+    kuns = [kun.strip() for kun in kuns]
+    kunyomis[kanji] = kuns
+
+print('ONYOMI = ' + repr(onyomis))
+print('KUNYOMI = ' + repr(kunyomis))
---
+++ yomisplit/__init__.py
@@ -0,0 +1,143 @@
+'''Given a kanji token and its reading, try to split the reading by kanji.
+
+TODO: suport for Unicode composing.
+'''
+
+import re
+
+from yomisplit.yomi import ONYOMI, KUNYOMI
+
+'''Japanese 'voicing' or daku transformations classes, including the /h/ sounds
+(which changes to /b/) and the 'half-daku' (/h/ -> /p/).'''
+DAKUON = {
+'か' : '[かが]',
+'き' : '[きぎ]',
+'く' : '[くぐ]',
+'け' : '[けげ]',
+'こ' : '[こご]',
+'さ' : '[さざ]',
+'し' : '[しじ]',
+'す' : '[すず]',
+'せ' : '[せぜ]',
+'そ' : '[そぞ]',
+'た' : '[ただ]',
+'ち' : '[ちぢ]',
+'つ' : '[つづ]',
+'て' : '[てで]',
+'と' : '[とど]',
+'は' : '[はばぱ]',
+'ひ' : '[ひびぴ]',
+'ふ' : '[ふぶぷ]',
+'へ' : '[へべぺ]',
+'ほ' : '[ほぼぽ]',
+}
+
+def japanese_matchreg(hiragana):
+    '''Builds regexp to match hiragana strings according to Japanese phonology.
+
+    The returned regexp tests whether another hiragana string is 'the same' as
+    the one provided, including in this category derivations from Japanese
+    morpho-phonological processes: sequential 'voicing' (rendaku) as well as
+    gemination (sokuon).
+    
+    That is, it adds 'dakuten, 'handakuten, and small-tsu', so that:
+        (TODO: doctest)
+        
+    '''
+    matchreg = ''
+
+    first = hiragana[0]
+    if first in DAKUON.keys():
+        matchreg += DAKUON[first]
+    else:
+        matchreg += first
+
+    matchreg += hiragana[1:-1] # skips last
+
+    if len(hiragana) > 1:
+        last = hiragana[-1]
+        # sokuon processing
+        if last in ['つ', 'ち']:
+            matchreg += '[%sっ]?' % last
+        else:
+            matchreg += last
+
+    return(matchreg)
+
+def japanese_match(hiragana1, hiragana2):
+    '''True if the two hiragana strings may be derived by Japanese phonology.
+
+    Tests whether the two hiragana strings are 'the same', including in this
+    category derivations from Japanese morpho-phonological processes:
+    sequential 'voicing' (rendaku) as well as gemination (sokuon).
+
+    That is, it adds 'dakuten', 'handakuten', and 'small-tsu', so that:
+        (TODO: doctest)
+
+    Wrapper on japanese_matchreg.
+    '''
+    matchreg = japanese_matchreg(hiragana1)
+    return(re.match(matchreg, hiragana2))
+            
+
+def yomi_matchreg(kanjistring):
+    """Builds regexp that matches possible known readings of kanjistring.
+
+    Use the regexp to match a reading later, in hiragana.  The resulting match
+    will include named groups, one for each kanji.
+    """
+    matchreg = ''
+    for kanji in kanjistring:
+        matchreg += '(?P<%s>' % kanji
+
+        found=False
+        if kanji in ONYOMI.keys():
+            found=True
+            onregs = [japanese_matchreg(on) for on in ONYOMI[kanji]]
+            matchreg += '|'.join(onregs)
+        if kanji in KUNYOMI.keys():
+            if found:
+                matchreg += '|'
+            else:
+                found=True
+            kunregs = [japanese_matchreg(kun) for kun in KUNYOMI[kanji]]
+            matchreg += '|'.join(kunregs)
+        if not found:
+            raise(ValueError("No reading found for kanji: '%s'" % kanji))
+
+        matchreg += ')'
+    return matchreg
+
+def canonical_reading(kanji, foundreading):
+    """Returns a 'canonical' reading (from Kanjidic).
+
+    E.g.:
+        >>> canonical_reading('花', 'ばな')
+        'はな'
+    """
+    creadings = []
+    if kanji in ONYOMI.keys():
+        creadings += ONYOMI[kanji]
+    if kanji in KUNYOMI.keys():
+        creadings += KUNYOMI[kanji]
+    for creading in creadings:
+        if japanese_match(creading, foundreading):
+            return(creading)
+
+def yomidict(kanji, reading):
+    m = re.match(yomi_matchreg(kanji), reading)
+    if (not m):
+        raise(ValueError("Could not find reading '%s' for kanji '%s'" %
+                         (reading, kanji)))
+    return(m.groupdict())
+
+def yomidict_canonical(kanji, reading):
+    d = yomidict(kanji, reading)
+    for kanji, found_reading in d.items():
+        d[kanji] = canonical_reading(kanji, found_reading)
+    return(d)
+
+
+
+def yomisplit(kanjiword, reading):
+    pass
